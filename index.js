5ffa9082c94079cfd2440de922f79e8f66442219
Antonio Scandurra <as-cii@github.com>
2017-06-05 16:42:40 +0200

    Fix app startup script
    Signed-off-by: Nathan Sobo <nathan@github.com>

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-require('dotenv').config()
 process.on('unhandledRejection', (reason) => {
   console.error(reason.stack)
 })
 
-const buildApp = require('./lib/app')
-const {app} = buildApp({databaseURL: process.env.DATABASE_URL})
-app.listen(process.env.PORT || 3000)
+require('dotenv').config()
+
+const pgp = require('pg-promise')()
+const buildControllerLayer = require('../lib/controller-layer')
+const PubSubGateway = require('../lib/pusher-pub-sub-gateway')
+
+const controllerLayer = buildControllerLayer({
+  db: pgp(process.env.DATABASE_URL),
+  pubSubGateway: new PubSubGateway({
+    appId: process.env.PUSHER_APP_ID,
+    key: process.env.PUSHER_KEY,
+    secret: process.env.PUSHER_SECRET
+  })
+})
+controllerLayer.listen(process.env.PORT || 3000)
