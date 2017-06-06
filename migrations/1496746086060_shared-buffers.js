529ae1e0c3b0ffd6353a1d413fa7c2e35e88ca15
Nathan Sobo <nathan@github.com>
2017-06-06 13:05:44 +0200

    Change migration library

---
+++ migrations/1496746086060_shared-buffers.js
@@ -0,0 +1,11 @@
+exports.up = (pgm) => {
+  pgm.createTable('shared_buffers', {
+    id: 'id',
+    last_site_id: {type: 'int', default: 1}
+  })
+  pgm.createTable('shared_buffer_operations', {
+    id: 'id',
+    data: {type: 'json'},
+    shared_buffer_id: {type: 'int', references: 'shared_buffers'}
+  })
+}
