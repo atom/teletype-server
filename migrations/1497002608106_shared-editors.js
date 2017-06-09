76a7eadbf251380af91060196d11ea0b9c1b038f
Nathan Sobo <nathan@github.com>
2017-06-09 15:08:41 +0200

    Support selection ranges in shared editors
    Signed-off-by: Max Brunsfeld <maxbrunsfeld@github.com>

--- migrations/1497002608106_shared-editors.js
+++ migrations/1497002608106_shared-editors.js
@@ -1,7 +1,7 @@
 exports.up = (pgm) => {
   pgm.createTable('shared_editors', {
     id: 'id',
-    scroll_position: {type: 'text'},
-    shared_buffer_id: {type: 'int', references: 'shared_buffers'}
+    shared_buffer_id: {type: 'int', references: 'shared_buffers'},
+    selection_ranges: {type: 'text'}
   })
 }
