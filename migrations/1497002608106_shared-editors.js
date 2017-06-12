46f3daf83574d96cc23bdd1d3e6ff9567246562d
Antonio Scandurra <as-cii@github.com>
2017-06-12 12:03:35 +0200

    Store a separate marker layer for each editor
    Signed-off-by: Nathan Sobo <nathan@github.com>

--- migrations/1497002608106_shared-editors.js
+++ migrations/1497002608106_shared-editors.js
@@ -1,7 +1,13 @@
 exports.up = (pgm) => {
   pgm.createTable('shared_editors', {
     id: 'id',
-    shared_buffer_id: {type: 'int', references: 'shared_buffers'},
-    selection_ranges: {type: 'text'}
+    shared_buffer_id: {type: 'int', references: 'shared_buffers'}
+  })
+
+  pgm.createTable('shared_editor_selection_marker_layers', {
+    id: 'id',
+    site_id: {type: 'int'},
+    shared_editor_id: {type: 'int', references: 'shared_editors'},
+    marker_ranges: {type: 'text'}
   })
 }
