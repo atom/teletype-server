72f7b6836c65f064a1a3696788fd7086cc8e7355
Nathan Sobo <nathan@github.com>
2017-07-31 17:03:23 -0600

    Fetch Twilio ICE servers on start and serve them to clients on request

--- index.js
+++ index.js
@@ -10,6 +10,8 @@ async function startServer () {
     pusherAppId: process.env.PUSHER_APP_ID,
     pusherKey: process.env.PUSHER_KEY,
     pusherSecret: process.env.PUSHER_SECRET,
+    twilioAccount: process.env.TWILIO_ACCOUNT,
+    twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
     port: process.env.PORT || 3000
   })
   await server.start()
