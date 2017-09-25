64934f7bf7cf2f7df27ef5fa1a931f27c80e6945
Antonio Scandurra <as-cii@github.com>
2017-09-25 17:14:20 +0200

    Rename boomtownSecret to healthCheckSecret

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ async function startServer (id) {
     pusherSecret: process.env.PUSHER_SECRET,
     twilioAccount: process.env.TWILIO_ACCOUNT,
     twilioAuthToken: process.env.TWILIO_AUTH_TOKEN,
-    boomtownSecret: process.env.BOOMTOWN_SECRET,
+    healthCheckSecret: process.env.HEALTH_CHECK_SECRET,
     port: process.env.PORT || 3000
   })
   await server.start()
