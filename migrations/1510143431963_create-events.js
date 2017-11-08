cd096029ebc47a2a1e6212414a0de6448d1317b2
Antonio Scandurra <as-cii@github.com>
2017-11-08 16:58:23 +0100

    Make events.name column an enum to reduce disk usage

--- migrations/1510143431963_create-events.js
+++ migrations/1510143431963_create-events.js
@@ -1,7 +1,8 @@
 exports.up = (pgm) => {
+  pgm.createType('event_enum', ['create-portal', 'lookup-portal'])
   pgm.createTable('events', {
     id: {type: 'bigserial', primaryKey: true},
-    name: {type: 'string'},
+    name: {type: 'event_enum'},
     user_id: {type: 'string'},
     portal_id: {type: 'uuid'},
     created_at: {type: 'timestamp with time zone'}
